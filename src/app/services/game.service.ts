0e5ad542f4694ca087ee04bb817043d2baba3b1e
Sarat <sarat2@outlook.com>
2018-04-25 16:29:38 -0400

    asd

---
+++ src/app/services/game.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class GameService {
+    private api = "http://localhost:8080/api/";
+    constructor(private http: Http) { }
+
+    getGames() {
+        return new Promise((resolve, reject) => {
+            this.http.get(this.api + "game").toPromise().then(res => {
+                console.log(res.json());
+                resolve(res.json());
+            }).catch(err => {
+                reject(err);
+            })
+        })
+    }
+
+    getGame(id?: string) {
+        return new Promise((resolve, reject) => {
+            this.http.get(this.api + "game/" + id).toPromise().then(res => {
+                console.log(res.json());
+                resolve(res.json());
+            }).catch(err => {
+                reject(err);
+            })
+        })
+    }
+
+}
